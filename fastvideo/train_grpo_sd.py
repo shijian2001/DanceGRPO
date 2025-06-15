beac38ec0325f5d8ce7be40a0b7b955d121952cd
Tomorrow1sanotherday <furunhao2000@163.com>
2025-06-15 22:24:10 +0800

    Add vqa_score reward

--- fastvideo/train_grpo_sd.py
+++ fastvideo/train_grpo_sd.py
@@ -260,43 +260,9 @@ def main(_):
     )
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # prepare prompt and reward fn
-    from hpsv2.src.open_clip import create_model_and_transforms, get_tokenizer
-    from typing import Union
-    import huggingface_hub
-    from hpsv2.utils import root_path, hps_version_map
-    def initialize_model():
-        model_dict = {}
-        model, preprocess_train, preprocess_val = create_model_and_transforms(
-            'ViT-H-14',
-            './hps_ckpt/open_clip_pytorch_model.bin',
-            precision='amp',
-            device=device,
-            jit=False,
-            force_quick_gelu=False,
-            force_custom_text=False,
-            force_patch_dropout=False,
-            force_image_size=None,
-            pretrained_image=False,
-            image_mean=None,
-            image_std=None,
-            light_augmentation=True,
-            aug_cfg={},
-            output_dict=True,
-            with_score_predictor=False,
-            with_region_predictor=False
-        )
-        model_dict['model'] = model
-        model_dict['preprocess_val'] = preprocess_val
-        return model_dict
-    model_dict = initialize_model()
-    model = model_dict['model']
-    preprocess_val = model_dict['preprocess_val']
-    #cp = huggingface_hub.hf_hub_download("xswu/HPSv2", hps_version_map["v2.1"])
-    cp = "./hps_ckpt/HPS_v2.1_compressed.pt"
-
-    checkpoint = torch.load(cp, map_location=f'cuda')
-    model.load_state_dict(checkpoint['state_dict'])
-    processor = get_tokenizer('ViT-H-14')
+  
+    import t2v_metrics
+    model = t2v_metrics.VQAScore(model='clip-flant5-xxl') # our recommended scoring model
     reward_model = model.to(device)
     reward_model.eval()
     
@@ -439,24 +405,9 @@ def main(_):
                     )
             rewards = []
             tuwen_rewards = []
-            for j, image in enumerate(images):
-                pil = Image.fromarray(
-                    (image.to(torch.float32).cpu().numpy().transpose(1, 2, 0) * 255).astype(np.uint8)
-                )
-                pil = pil.resize((512, 512))
-                image_path = os.path.join("./images_same", f"image-{i}-{j}-rank-{dist.get_rank()}.jpg")
-                pil.save(image_path)
-                image = preprocess_val(Image.open(image_path).convert("RGB")).unsqueeze(0).to(device=device, non_blocking=True)
-                # Process the prompt
-                text = processor([current_batch[j]]).to(device=device, non_blocking=True)
-                # Calculate the HPS
-                with torch.no_grad():
-                    with torch.amp.autocast('cuda'):
-                        outputs = reward_model(image, text)
-                        image_features, text_features = outputs["image_features"], outputs["text_features"]
-                        logits_per_image = image_features @ text_features.T
-                        hps_score = torch.diagonal(logits_per_image)
-                    rewards.append(hps_score)
+
+            vqa_scores = torch.diagonal(reward_model(images=images, texts=current_batch))
+            rewards.append(vqa_scores)
 
 
 
